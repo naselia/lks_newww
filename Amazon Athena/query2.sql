1b0f7313a8cfea23382b6892577132db9f728398
louis <louisfransisco026@gmail.com>
2026-06-25 13:00:06 +0700

    Update query2.sql

--- Amazon Athena/query2.sql
+++ Amazon Athena/query2.sql
@@ -2,8 +2,10 @@ CREATE EXTERNAL TABLE IF NOT EXISTS lks_analytics.transactions (
     transaction_id string;
     user_id string;
     amount bigint;
-    status_ai bigint;
-    processed_at bigint
+    pajak_11 bigint;
+    total_billing bigint;
+    status_ai string;
+    processed_at string
 )
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 LOCATION 's3://transaction.buckets/raw-transactions';
